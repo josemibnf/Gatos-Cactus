584c6dbcbbc6a9d537d91a099d454f0a04b62007
jal04 <josemi.bnf@outlook.es>
2020-05-23 15:37:10 +0200

    WhatIsIt added

---
+++ lib/WhatIsIt.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WhatIsIt extends StatefulWidget{
+  Future<WhatIsItState> createState() async => WhatIsItState();
+  }
+  
+  class WhatIsItState {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:hello_world/RandomWords.dart';
+import 'package:hello_world/WhatIsIt.dart';
 
 void main() => runApp(MaterialApp(home: BottomNavBar()));
 
@@ -12,64 +13,65 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   // all pages
   final RandomWords _randomWords = RandomWords();
-  Widget _showPage;  
-  Widget _choosePage(int page){
-    switch (page){
-      case 0:
-        return _randomWords;
-        break;
-      case 1:
-        return _randomWords;
-        break;
-      case 2:
-        return _randomWords;
-        break;
-      case 3:
-        return _randomWords;
-        break;
-      case 4:
-        return _randomWords;
-        break;
-      case 5:
-        return _randomWords;
-        break;
-      default:
-        return new Container(
-          child: new Center(
-            child: new Text(
-              "No hay mas paginas o que ??",
-              style: new TextStyle(fontSize: 30)
+  final WhatIsIt _whatisit = WhatIsIt();
+    Widget _showPage;  
+    Widget _choosePage(int page){
+      switch (page){
+        case 0:
+          return _randomWords;
+          break;
+        case 1:
+          return _randomWords;
+          break;
+        case 2:
+          return _randomWords;
+          break;
+        case 3:
+          return _randomWords;
+          break;
+        case 4:
+          return _randomWords;
+          break;
+        case 5:
+          return _whatisit;
+          break;
+        default:
+          return new Container(
+            child: new Center(
+              child: new Text(
+                "No hay mas paginas o que ??",
+                style: new TextStyle(fontSize: 30)
+              )
             )
-          )
+          );
+      }
+    }
+    
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          bottomNavigationBar: CurvedNavigationBar(
+            height: 53.0,
+            items: <Widget>[
+              Icon(Icons.add, size: 30),
+              Icon(Icons.list, size: 30),
+              Icon(Icons.compare_arrows, size: 30),
+              Icon(Icons.call_split, size: 30),
+              Icon(Icons.perm_identity, size: 30),
+              Icon(Icons.live_tv, size: 30),
+            ],
+            color: Colors.yellow[300],
+            buttonBackgroundColor: Colors.yellow[300],
+            backgroundColor: Colors.transparent,
+            animationCurve: Curves.easeInOut,
+            animationDuration: Duration(milliseconds: 600),
+            onTap: (int tappIndex) {
+              setState(() {
+                _showPage = _choosePage(tappIndex);
+              });
+            },
+          ),
+          body: _showPage
         );
     }
   }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        bottomNavigationBar: CurvedNavigationBar(
-          height: 53.0,
-          items: <Widget>[
-            Icon(Icons.add, size: 30),
-            Icon(Icons.list, size: 30),
-            Icon(Icons.compare_arrows, size: 30),
-            Icon(Icons.call_split, size: 30),
-            Icon(Icons.perm_identity, size: 30),
-            Icon(Icons.live_tv, size: 30),
-          ],
-          color: Colors.yellow[300],
-          buttonBackgroundColor: Colors.yellow[300],
-          backgroundColor: Colors.transparent,
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 600),
-          onTap: (int tappIndex) {
-            setState(() {
-              _showPage = _choosePage(tappIndex);
-            });
-          },
-        ),
-        body: _showPage
-      );
-  }
-}
