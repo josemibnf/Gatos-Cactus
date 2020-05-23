bb0b0c00a9ce295536078eb3a5ab72d224708ec7
jal04 <josemi.bnf@outlook.es>
2020-05-23 16:16:32 +0200

    create methods

--- lib/WhatIsIt.dart
+++ lib/WhatIsIt.dart
@@ -1,12 +1,32 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
 
 class WhatIsItState extends State<WhatIsIt> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    load_model();
+    image_classifier();
+    close_model();
     return null;
   }
 
+  Future<void> load_model() async {
+    String res = await Tflite.loadModel(
+      model: "assets/mobilenet_v1_1.0_224.tflite",
+      labels: "assets/labels.txt",
+      numThreads: 1 // defaults to 1
+    );
+  }
+
+  Future<Void> image_classifier() async {
+    
+  }
+
+  Future<Void> close_model() async {
+    await Tflite.close();
+  }
 }
 
 class WhatIsIt extends StatefulWidget{
